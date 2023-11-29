c1d08dc9aa4f0c8a11634a96dbaa03c2be6acef7
teddunderz70765 <152019995+teddunderz70765@users.noreply.github.com>
2023-11-29 18:54:20 +0000

    added dog type if statement

---
+++ PetProgram.py
@@ -0,0 +1,8 @@
+print("what kind of pet do you have")
+pet = input()
+print("you have a pet " + pet)
+
+if pet == "dog":
+    print("what kind of dog do you have?")
+    dog_type = input()
+    print("you have a " + dog_type)
--- testFile.py
+++
@@ -1,3 +0,0 @@
-print("what kind of pet do you have")
-pet = input()
-print("you have a pet " + pet)
