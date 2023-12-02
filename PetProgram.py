f18873abba26e9a771c3b0cdb60a8bb7f4d4c248
teddunderz70765 <152019995+teddunderz70765@users.noreply.github.com>
2023-12-02 15:58:05 +0000

    added cat and hamster if statements

--- PetProgram.py
+++ PetProgram.py
@@ -6,3 +6,14 @@ if pet == "dog":
     print("what kind of dog do you have?")
     dog_type = input()
     print("you have a " + dog_type)
+
+if pet == "cat":
+    print("what colour cat do you have?")
+    cat_colour = input()
+    print("your cat is the colour " + cat_colour)
+
+if pet == "hamster":
+    print("how old is your hamster?")
+    hamster_age = input()
+    print("your hamster age is " + hamster_age)
+
